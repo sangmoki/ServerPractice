f2c8d12e077cc9bfe266b036e82ee4c73eeb9f0f
sangmoki <yoochang4176@naver.com>
2024-10-05 20:53:32 +0900

    Feat: "Thread 백그라운드 및 Naming, Join문 사용"

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using System;
+using System.Threading;
 
 namespace Program
 {
@@ -6,13 +7,25 @@ namespace Program
     {
         static void MainThread()
         {
-            Console.WriteLine("Hello Thread!");
+            while(true)
+                Console.WriteLine("Hello Thread!");
         }
 
         static void Main(string[] args)
         {
             Thread t = new Thread(MainThread);
+
+            t.Name = "Test Thread";
+            // 백그라운드에서 실행할 것인지 설정 (기본값은 false)
+            t.IsBackground = true;
+
+            // thread 시작
             t.Start();
+
+            Console.WriteLine("Wating for Thread!");
+
+            // thread가 종료될 때까지 대기
+            t.Join();
             Console.WriteLine("Hello World!");
         }
     }
