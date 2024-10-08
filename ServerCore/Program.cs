566950e35366d242de9d57838f296fa97f983819
sangmoki <yoochang4176@naver.com>
2024-10-08 13:10:39 +0900

    Feat: "Thread와 ThreadPool의 장점을 살린 Task 사용"

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -15,13 +15,23 @@ namespace Program
         {
             // 최소 실행 쓰레드
             ThreadPool.SetMinThreads(1, 1);
-
             // 최대 실행 쓰레드
             ThreadPool.SetMaxThreads(5, 5);
 
+            // 쓰레드랑은 다르지만 비슷한 역할을 하는 Task
+            // 뒤에 TaskCreationOptions와 같은 option을 통하여 별도의 thread를 실행한다.
+            // Thread와 ThreadPool의 장점을 뽑아서 사용하는 느낌
             for (int i = 0; i < 5; i++)
-                ThreadPool.QueueUserWorkItem((obj) => {  while (true) { } });
+            {
+                // 오래걸리는 작업을 할떄 LongRunning 사용 
+                Task t = new Task(() => { while (true) { } }, TaskCreationOptions.LongRunning);
+                t.Start();
+            }
 
+/*
+            for (int i = 0; i < 4; i++)
+                ThreadPool.QueueUserWorkItem((obj) => {  while (true) { } });
+*/
             // ThreadPool 테스트 -> background로 실행
             ThreadPool.QueueUserWorkItem(MainThread);
 
