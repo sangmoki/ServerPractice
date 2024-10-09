fbca44c70edc6faefd4eb4e1da7f072d254e8e82
sangmoki <yoochang4176@naver.com>
2024-10-10 08:05:55 +0900

    Feat: "멀티 쓰레드 환경에서의 발생할 수 있는 문제점 -> 하드웨어 자체에서 최적화로 인한 순서 뒤바뀜"

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -5,31 +5,75 @@ namespace Program
 {
     class Program
     {
+        static volatile int x = 0;
+        static volatile int y = 0;
+        static volatile int r1 = 0;
+        static volatile int r2 = 0;
+
+        static void Thread_1()
+        {
+            y = 1; // Store y
+            r1 = x; // Load x
+        }        
+
+        static void Thread_2()
+        {
+            x = 1; // Store x
+            r2 = y; // Load y
+        }
 
         static void Main(string[] args)
         {
-            int[,] arr = new int[10000, 10000];
+            int count = 0;
+            while(true)
             {
-                long now = DateTime.Now.Ticks;
-                for (int y = 0; y < 10000; y++)
-                    for (int x = 0; x < 10000; x++)
-                        arr[y, x] = 1;
+                count++;
+                x = y = r1 = r2 = 0;
 
-                long end = DateTime.Now.Ticks;
-                Console.WriteLine($"(y, x) 순서 걸린 시간 {end - now}");
+                // 멀티 쓰레드 환경
+                Task t1 = new Task(Thread_1);
+                Task t2 = new Task(Thread_2);
+                t1.Start();
+                t2.Start();
+
+                Task.WaitAll(t1, t2);
+
+                // 멀티쓰레드 환경에서는 
+                // 하드웨어 자체에서 최적화를 하기 때문에 -? 순서를 뒤바꾸기도 한다.
+                // 그래서 r1, r2가 0이 나올 수 있다.
+                if (r1 == 0 && r2 == 0)
+                    break;
             }
 
-            {
-                long now = DateTime.Now.Ticks;
-                for (int y = 0; y< 10000; y++)
-                    for (int x = 0; x< 10000; x++)
-                        arr[x, y] = 1;
+            Console.WriteLine($"{count}번 만에 빠져나옴");
 
-                long end = DateTime.Now.Ticks;
-                Console.WriteLine($"(x, y) 순서 걸린 시간 {end - now}");
-            }
         }
 
+        /* 캐시 테스트 전 2차배열 하드코딩
+                static void Main(string[] args)
+                {
+                    int[,] arr = new int[10000, 10000];
+                    {
+                        long now = DateTime.Now.Ticks;
+                        for (int y = 0; y < 10000; y++)
+                            for (int x = 0; x < 10000; x++)
+                                arr[y, x] = 1;
+
+                        long end = DateTime.Now.Ticks;
+                        Console.WriteLine($"(y, x) 순서 걸린 시간 {end - now}");
+                    }
+
+                    {
+                        long now = DateTime.Now.Ticks;
+                        for (int y = 0; y< 10000; y++)
+                            for (int x = 0; x< 10000; x++)
+                                arr[x, y] = 1;
+
+                        long end = DateTime.Now.Ticks;
+                        Console.WriteLine($"(x, y) 순서 걸린 시간 {end - now}");
+                    }
+                }
+        */
 
         /* 최적화로 인한 문제 해결 방안
                 // 전역으로 설정되어 모든 Thread 들이 사용 가능하다.
